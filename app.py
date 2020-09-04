11f3beeea79831d33ecce5db9aef0349eb852927
Tyler Blazey <blazeyt@MacBook-Air.local>
2020-09-03 22:18:57 -0500

    test

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ fig = px.scatter(plot_data,
                  color_discrete_sequence=['#348cc1', '#e34e21'])
                  
 #Update layout
+fig.update_traces(marker={'size':12, 'line':{'color':'black', 'width':2}})
 fig.update_xaxes(showgrid=True,
                  gridwidth = 1,
                  gridcolor = 'Black',
@@ -48,6 +49,8 @@ fig.update_yaxes(showgrid = True,
                  range = [0.0, 2.5],
                  fixedrange = True)
 fig.update_layout(dragmode = 'pan',
+                  margin={'t':40, 'b':40},
+                  autosize=True,
                   plot_bgcolor = 'white',
                   xaxis_title = '<b>Date</b>',
                   xaxis_title_font_color = 'black',
@@ -55,26 +58,52 @@ fig.update_layout(dragmode = 'pan',
                   yaxis_title= '<b>Temperature (K)</b>',
                   yaxis_title_font_color = 'black',
                   yaxis_title_font_size = 20,
-                  title = '<b>Spinlab Temperature</b>',
-                  title_font_color = 'black',
-                  title_font_size = 24,
-                  title_x = 0.5,
-                  legend={'y' : 0.5,
+                  legend={'y' : 0.1,
                          'title' : '<b>Status</b>',
                          'xanchor' : 'center',
                          'bgcolor' : 'rgba(255, 255, 255, 1)',
                          'bordercolor' : 'black',
                          'borderwidth' : 1.5,
                          'title_font_size' : 16,
+                         'title_font_color' : 'black',
                          'font':{'color' :'black', 'size' : 14}})
 
 #Create dash app for showing plot
 config = dict({'scrollZoom': True})
 app = dash.Dash(title='Spinlab Temperature')
 server = app.server
-app.layout = html.Div([
-    dcc.Graph(figure=fig, config=config)
-])
+app.layout = html.Div(
+    children=[
+        html.Div(className='rows', style={'columnCount':2},
+            children=[
+                html.Div(className='one columns', style={'width':'100%'},
+                    children=[
+                        html.H1(style={'text-align': 'center', 'font-size':'1.75vw'},
+                            children=[
+                                'Spinlab Temperature Log',
+                                dcc.Graph(figure=fig, config=config, style={'height': '40vw'})
+                            ]
+                        )
+                    ]
+                ),
+                html.Div(className='one columns', style={'display': 'inline-block', 'width':'95%'},
+                    children=[
+                        html.H1(style={'text-align': 'center', 'font-size':'1.75vw'},
+                            children=[
+                                'Latest CryoStatus Page',
+                                html.Img(src=app.get_asset_url('2020-08-28-18-56-27.png'),
+                                         style={'max-width': '100%',
+                                                'max-height':'100%',
+                                                'padding-top':'2vw',
+                                                'padding-left':'2vw'})
+                            ]
+                        )                     
+                    ]
+                )
+            ]
+        )
+    ]
+)
 
 #Run app
 if __name__ == '__main__':
